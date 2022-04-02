58da8ea7f09b2ffad7e981c4e2a24579a68a6d0a
thisiszheng <brianz12@nycstudents.net>
2022-04-02 13:57:21 -0400

    Enter

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+  /**
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
+   */
+
+  private Spark leftMotorFront = new Spark(3);
+  private Spark leftMotorBack = new Spark(4);
+  private Spark rightMotorFront = new Spark(1);
+  private Spark rightMotorBack = new Spark(2);
+
+  private Joystick joy1 = new Joystick(0);
+
+  @Override
+  public void robotInit() {}
+
+  @Override
+  public void robotPeriodic() {}
+
+  @Override
+  public void autonomousInit() {}
+
+  @Override
+  public void autonomousPeriodic() {}
+
+  @Override
+  public void teleopInit() {}
+
+  @Override
+  public void teleopPeriodic() {
+    double speed = -joy1.getRawAxis(1) * 0.6;
+    double turn = joy1.getRawAxis(2) * 0.3;
+
+    double left = speed + turn;
+    double right = speed - turn;
+
+    leftMotorFront.set(left);
+    leftMotorBack.set(left);
+    rightMotorFront.set(-right);
+    rightMotorBack.set(-right);
+  }
+
+  @Override
+  public void disabledInit() {}
+
+  @Override
+  public void disabledPeriodic() {}
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
+}
